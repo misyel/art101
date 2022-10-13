341fc53f79510cb5cfe137a8ff74737c09e27440
Michelle Kwong <michellekwong@Michelles-MacBook-Pro.local>
2022-10-12 19:57:21 -0700

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// Summary. 
+// Description.
+// @link   URL
+// @file   This file contains the psuedocode comments for lab4.
+// @author Michelle Kwong.
+// @since  10/12/2022
+
+
+// Have player click rock paper or scissors button
+// Evaluate player’s move
+// Computer randomly selects rock paper or scissors 
+// Evaluate computer’s move
+// Evaluate computer and player result 
+// Display result to player

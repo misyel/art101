403b8db560d2695920cc5c976e3d6f51b04523ab
Michelle Kwong <michellekwong@Michelles-MacBook-Pro.local>
2022-10-16 14:38:44 -0700

    lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author: Michelle Kwong (mkwong6@ucsc.edu)
+ * Created: 10/16/2022
+ * License: Public Domain
+ */
+
+// Define Variables
+const make = "Toyota";
+const model = "Prius";
+const color = "Gray";
+const year = 2004;
+
+// Calculate Age
+const age = 2022 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: ", age + " years <br>");
+

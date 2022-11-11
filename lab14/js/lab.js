ea054e5e2b042f60bb18bfb961f79e3c10fe12cc
Michelle Kwong <michellekwong@Michelles-MacBook-Pro.local>
2022-11-11 15:03:45 -0800

    lab14

---
+++ lab14/js/lab.js
@@ -0,0 +1,10 @@
+/**
+ * Author: Michelle Kwong (mkwong6@ucsc.edu)
+ * Created: 11/11/2022
+ * License: Public Domain
+ */
+
+const output = document.getElementById("output");
+const p = document.createElement("p");
+p.innerHTML = "Debugger running :)";
+output.appendChild(p);

3707623f06c2887dba2054cc1cb3d3095825469d
Michelle Kwong <michellekwong@Michelles-MacBook-Pro.local>
2022-11-28 21:08:52 -0800

    lab16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,13 +1,12 @@
 /**
  * Author: Michelle Kwong (mkwong6@ucsc.edu)
- * Created: 11/21/2022
+ * Created: 11/28/2022
  * License: Public Domain
  */
 
 const getData = async () => { 
     const data = await fetch('https://xkcd.com/614/info.0.json');
     const json = await data.json();
-    console.log(json);
 
     const h1 = document.createElement('h1');
     const img = document.createElement('img');
